5f46f8c17cba6e809d1ac70c743e955463b1bd54
maksim1212 <mr.frost201611@gmail.com>
2020-03-03 15:39:50 +0200

    feat(screen.js): save image link to txt file
    was added random image name generator and saving image link to txt file

--- src/tmp/screen.js
+++ src/tmp/screen.js
@@ -1,28 +1,50 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-// const path = require('path');
-// const prompt = require('prompt');
 const fetch = require('isomorphic-fetch');
 const { Dropbox } = require('dropbox');
 
-const websiteAddress = 'http://localhost:3000/v1/users';
-const imageName = 'screen.png';
-const accessTokenData = 'q24GClcTzDAAAAAAAAAAJwhVUMlqe2MUgQKU8soshKktOUHZjDMrKhsGgYkGaWf9';
-const dbx = new Dropbox({ accessToken: accessTokenData, fetch });
-const imagePath = `images/${imageName}`;
+/**
+ * @function
+ * @param max
+ * @param min
+ * @returns random integer number
+ */
+function randomInteger(min, max) {
+    const rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
 
+const defaultName = randomInteger(1, 10000); // random image name
+const websiteAddress = 'http://localhost:3000/v1/users';
+const imageFormat = '.png'; // defaul image format
+const imageName = defaultName + imageFormat; // image file name + format
+const accessTokenData = 'q24GClcTzDAAAAAAAAAALqIk0QTTnjJguMXE2RdKlsL-KJcg26rXWa4M5kt4FXc6';
+const dbx = new Dropbox({ accessToken: accessTokenData, fetch });
+
+// open headless chrome,go to website, make screenshot, sent to Dropbox and write link to .txt file
 (async() => {
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
+
         await page.goto(websiteAddress);
-        await page.screenshot({ path: imagePath });
-        //  await page.pdf({ path: 'first.pdf', format: 'A4' });
-        // console.log(dbx);
-        await dbx.filesUpload({ path: `/puppeteer/${imagePath}`, contents: fs.createReadStream(imagePath) });
-        // const sharedData = await dbx.sharingCreateSharedLinkWithSettings({ path: `/puppeteer/${imagePath}` });
-        //  await sharedData.pdf({ path: 'url.pdf', format: 'A4' });
-        //  console.log(sharedData);
+        await page.screenshot({ path: imageName }); // make screenshot image
+        await dbx.filesUpload({ path: `/${imageName}`, contents: fs.createReadStream(imageName) }); // upload image to Dropbox
+        console.log(`image ${imageName} uploaded successfully `);
+
+        const sharedData = await dbx.sharingCreateSharedLinkWithSettings({ path: `/${imageName}` });
+        const imageUrl = sharedData.url; // image url for Dropbox
+
+        // write image url to 'dataUrl.txt'
+        fs.writeFile('dataUrl.txt', imageUrl, (error) => {
+            if (error) throw error;
+        });
+
+        // remove image from server
+        fs.unlink(imageName, (error) => {
+            if (error) throw error;
+        });
+
         await browser.close();
     } catch (error) {
         console.log(error);
