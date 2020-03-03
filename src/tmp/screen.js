b298f4b79ec111e577e0edd45240778c07e2285f
maksim1212 <mr.frost201611@gmail.com>
2020-03-03 11:12:07 +0200

    feat(screen.js): make a screen and sent to drop box

--- src/tmp/screen.js
+++ src/tmp/screen.js
@@ -1,10 +1,30 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+// const path = require('path');
+// const prompt = require('prompt');
+const fetch = require('isomorphic-fetch');
+const { Dropbox } = require('dropbox');
+
+const websiteAddress = 'http://localhost:3000/v1/users';
+const imageName = 'screen.png';
+const accessTokenData = 'q24GClcTzDAAAAAAAAAAJwhVUMlqe2MUgQKU8soshKktOUHZjDMrKhsGgYkGaWf9';
+const dbx = new Dropbox({ accessToken: accessTokenData, fetch });
+const imagePath = `images/${imageName}`;
 
 (async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/v1/users');
-    await page.screenshot({ path: 'screen.png' });
-
-    await browser.close();
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(websiteAddress);
+        await page.screenshot({ path: imagePath });
+        //  await page.pdf({ path: 'first.pdf', format: 'A4' });
+        // console.log(dbx);
+        await dbx.filesUpload({ path: `/puppeteer/${imagePath}`, contents: fs.createReadStream(imagePath) });
+        // const sharedData = await dbx.sharingCreateSharedLinkWithSettings({ path: `/puppeteer/${imagePath}` });
+        //  await sharedData.pdf({ path: 'url.pdf', format: 'A4' });
+        //  console.log(sharedData);
+        await browser.close();
+    } catch (error) {
+        console.log(error);
+    }
 })();
