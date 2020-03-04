26f1855c3ca775f81e7aa909561e89cc249b231c
maksim1212 <mr.frost201611@gmail.com>
2020-03-04 19:50:44 +0200

    style(puppeteer.js): added comments
    was added comments

--- src/tmp/puppeteer.js
+++ src/tmp/puppeteer.js
@@ -7,26 +7,29 @@ const websiteAddress = 'http://localhost:3000/v1/users';
     try {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
+
         await page.goto(websiteAddress);
-        await page.waitForSelector('tbody');
+        // await page.waitForSelector('tbody');
 
         const result = await page.evaluate(() => {
-            const dataAll = document.querySelectorAll('td');
+            const dataAll = document.querySelectorAll('td'); // get data that are in tags 'td'
             const dataArr = [];
 
             dataAll.forEach((td) => {
-                dataArr.push(td.innerText);
+                dataArr.push(td.innerText); // write 'td' data to array
             });
 
             return dataArr;
         });
 
-        const emails = await result.filter(str => str.match(/@/g));
-        console.log(emails);
+        const emails = await result.filter(str => str.match(/@/g)); // sort emails from extra data
+        console.log(`Grabbing Emails : ${emails}`);
+
         emails.forEach(async(email) => {
             await GrabbingModel.create({ email }); // wrtite email data to db
         });
 
+        console.log('Emails successfully written to the database');
         await browser.close();
     } catch (error) {
         console.log(error);
